e8399b720ac0fc70590c488a9eee0d467b26fe01
Xavier Drdak <1198051+xdrdak@users.noreply.github.com>
2019-07-08 18:42:20 -0400

    move business logic to the store

--- src/components/MonthSwitcher.svelte
+++ src/components/MonthSwitcher.svelte
@@ -1,57 +1,18 @@
 <script>
-  import { month, year } from "../store/time";
-
-  // TODO: This is garbage. Move business logic to store
-  let nextMonth = 0;
-  let previousMonth = 0;
-  let nextYear = 0;
-  let previousYear = 0;
-
-  year.subscribe(y => {
-    nextYear = y + 1;
-    previousYear = y - 1;
-  });
-
-  month.subscribe(m => {
-    if (m < 11) {
-      nextMonth = m + 1;
-    } else {
-      nextMonth = 0;
-    }
-
-    if (m === 0) {
-      previousMonth = 11;
-    } else {
-      previousMonth = m - 1;
-    }
-  });
-
-  function setNextMonth() {
-    month.set(nextMonth);
-    if (nextMonth === 0) {
-      year.set(nextYear);
-    }
-  }
-
-  function setPreviousMonth() {
-    month.set(previousMonth);
-    if (previousMonth === 11) {
-      year.set(previousYear);
-    }
-  }
+  import { gotoNextMonth, gotoPrevMonth } from "../store/time";
 </script>
 
 <div class="flex">
   <button
     style="cursor: pointer;"
     class="pv2 ph3 ma0 bn"
-    on:click={setPreviousMonth}>
+    on:click={gotoPrevMonth}>
     ◀
   </button>
   <button
     style="cursor: pointer;"
     class="pv2 ph3 ma0 bn"
-    on:click={setNextMonth}>
+    on:click={gotoNextMonth}>
     ▶
   </button>
 </div>
--- src/store/time.js
+++ src/store/time.js
@@ -4,4 +4,33 @@ import { today } from '../utils/constants';
 const year = writable(today.getFullYear());
 const month = writable(today.getMonth());
 
-export { year, month };
+const gotoNextYear = () => {
+  year.update(y => y + 1);
+};
+
+const gotoPrevYear = () => {
+  year.update(y => y - 1);
+};
+
+const gotoNextMonth = () => {
+  month.update(m => {
+    if (m < 11) {
+      return m + 1;
+    }
+    gotoNextYear();
+    return 0;
+  });
+};
+
+const gotoPrevMonth = () => {
+  month.update(m => {
+    if (m === 0) {
+      gotoPrevYear();
+      return 11;
+    }
+
+    return m - 1;
+  });
+};
+
+export { year, month, gotoNextMonth, gotoPrevMonth };
